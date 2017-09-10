c8fa1dc3e45994f2e08ca71e90387c51bf3d7e7d
Menno van Grinsven <scanno71@gmail.com>
2017-09-10 22:19:06 +0200

    Disable the inclusion of libgpustats for msm8996

--- mm-core/Android.mk
+++ mm-core/Android.mk
@@ -104,7 +104,7 @@ LOCAL_VENDOR_MODULE     := true
 LOCAL_SHARED_LIBRARIES  := liblog libdl libcutils
 LOCAL_CFLAGS            := $(OMXCORE_CFLAGS)
 
-ifneq (,$(filter msm8996 msm8998 apq8098_latv sdm660,$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8998 apq8098_latv sdm660,$(TARGET_BOARD_PLATFORM)))
 LOCAL_SHARED_LIBRARIES  += libgpustats
 endif
 
@@ -133,7 +133,7 @@ LOCAL_VENDOR_MODULE     := true
 LOCAL_SHARED_LIBRARIES  := liblog libdl libcutils
 LOCAL_CFLAGS            := $(OMXCORE_CFLAGS)
 
-ifneq (,$(filter msm8996 msm8998 apq8098_latv sdm660,$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8998 apq8098_latv sdm660,$(TARGET_BOARD_PLATFORM)))
 LOCAL_SHARED_LIBRARIES  += libgpustats
 endif
 
